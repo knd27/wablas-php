a40ae6d1f8433f8cb63d9e8051f0c3b3cdde7b78
kannyd <kannyd@gmail.com>
2021-02-21 17:01:59 +0700

    class 1

--- src/Wablas.php
+++ src/Wablas.php
@@ -5,14 +5,41 @@ namespace knd27\Wablas;
 class Wablas
 {
     protected $token;
+    protected $domain_api = "https://cepogo.wablas.com";
 
-    public function __construct($token)
+    public function __construct($token, $domain_api = null)
     {
         $this->token    = $token;
+        if (isset($domain_api)) {
+            $this->domain_api   = $domain_api;
+        }
     }
 
     public function tes()
     {
-        return $this->token;
+        return $this->domain_api;
+    }
+
+    private function _curl($url, $data)
+    {
+        $curl = curl_init();
+        curl_setopt(
+            $curl,
+            CURLOPT_HTTPHEADER,
+            array(
+                "Authorization: " . $this->token,
+            )
+        );
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        echo "<pre>";
+        print_r($result);
     }
 }
